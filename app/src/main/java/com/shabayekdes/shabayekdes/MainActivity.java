229023ce9812ac93cc4333a6c83f72358a907d3e
Esmail Shabayek <esmail.shabayek@gmail.com>
2017-03-07 03:47:31 +0200

    Add Main Menu

--- app/src/main/java/com/shabayekdes/shabayekdes/MainActivity.java
+++ app/src/main/java/com/shabayekdes/shabayekdes/MainActivity.java
@@ -3,6 +3,8 @@ package com.shabayekdes.shabayekdes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,7 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.exit)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
     public void btnClick(View view) {
 
         switch (view.getId()){
